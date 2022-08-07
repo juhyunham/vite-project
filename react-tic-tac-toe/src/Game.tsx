838d6d0545e88177e63a0e28c360892c4f1a5b8e
hamju <judev0227@gmail.com>
2022-08-07 18:53:47 +0900

    tailwind 적용 - npm install -D tailwindcss postcss autoprefixer

---
+++ react-tic-tac-toe/src/Game.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Game() {
+  return (
+    <div className="h-full p-8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500">
+      <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe</h1>
+    </div>
+  );
+}
+
+export default Game;
